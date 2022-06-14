db927b08332e1279dfb62b86db87b7748f05f926
Studio Kaori <studiokaori.com@gmail.com>
2022-06-14 21:05:12 +0200

    Initial Commit

---
+++ SwiftUIApiCall/SwiftUIApiCallApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIApiCallApp.swift
+//  SwiftUIApiCall
+//
+//  Created by Kaori Persson on 2022-06-14.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIApiCallApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
